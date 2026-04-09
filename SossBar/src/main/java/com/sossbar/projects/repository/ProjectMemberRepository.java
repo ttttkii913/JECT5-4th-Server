1ee0bc6e4652c132e9ffc41a315d2f59f74ea48d
choeunbin03 <choeunbiin03@naver.com>
2026-04-09 20:57:44 +0900

    Feat/#4: ProjectMemberRepository 수정 - deleteAllByProject 쿼리 최적화

--- SossBar/src/main/java/com/sossbar/projects/repository/ProjectMemberRepository.java
+++ SossBar/src/main/java/com/sossbar/projects/repository/ProjectMemberRepository.java
@@ -3,8 +3,13 @@ package com.sossbar.projects.repository;
 import com.sossbar.projects.entity.Project;
 import com.sossbar.projects.entity.ProjectMember;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 public interface ProjectMemberRepository extends JpaRepository<ProjectMember, Long> {
 
-    void deleteAllByProject(Project project);
+    @Modifying
+    @Query("delete from ProjectMember pm where pm.project = :project")
+    void deleteAllByProject(@Param("project") Project project);
 }
